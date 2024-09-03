44634983d0c4d252c663861b6b1aaf43e980c66c
SMZR_Dev <smzrdev@gmail.com>
2024-09-03 16:17:03 +0500

    Project Initialized

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black h-[100vh]">
+      <p className="text-white bg-black shadow-md">Hello World!</p>
+    </div>
+  );
+};
+
+export default App;
