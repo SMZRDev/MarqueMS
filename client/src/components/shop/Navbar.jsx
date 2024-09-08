dcf2e313ab96677a510419440d3790a5b48387ec
SMZR_Dev <smzrdev@gmail.com>
2024-09-09 04:23:50 +0500

    trying Admin Sidebar

--- client/src/components/shop/Navbar.jsx
+++ client/src/components/shop/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         </div>
       </nav>
       <nav className="bg-gray-50 dark:bg-gray-700">
-        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+        <div className=" max-w-screen-xl px-4 py-3 mx-auto">
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
